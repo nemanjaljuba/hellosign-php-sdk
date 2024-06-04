b86895dd9cbbec767e02a7a1d3815d0b535f3797
Nemanja Ljubotina <nemanjaljuba@yahoo.com>
2024-06-04 15:22:44 +0200

    Update SignatureRequest.php

--- library/HelloSign/SignatureRequest.php
+++ library/HelloSign/SignatureRequest.php
@@ -148,6 +148,8 @@ class SignatureRequest extends AbstractSignatureRequest
      */
     protected $form_fields_per_document = array();
 
+    public $created_at;
+    
     /**
      * Constructor
      *
